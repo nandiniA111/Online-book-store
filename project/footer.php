571427198b5d6345cfb1b0567ed608a7654c63e6
nandiniA111 <124701500+nandiniA111@users.noreply.github.com>
2023-05-18 21:18:04 +0530

    Add files via upload

---
+++ project/footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About us</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact us</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 7894561231 </p>
+         <p> <i class="fas fa-envelope"></i> john@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Chennai, India - 515104 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>Miss. Abc</span> </p>
+
+</section>
